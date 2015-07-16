af686de6b5262ac59de39baf451da508b7c90f03
bijesse <bijesse.thomas@gmail.com>
2015-07-16 19:00:56 +0000

    added form

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ SC.initialize({
 });
 
 $(document).ready(function() {
+  var searchButton =$("#searchButton");
+  searchButton.click(function(){
+  var userChoice = $("#gensearch").val();
 
-  SC.get('/tracks', {genres: 'punk'}, function(tracks) {
+  SC.get('/tracks', {genres: userChoice}, function(tracks) {
     $(tracks).each(function(index, track) {
-      $('#results').append($('<li></li>').html(track.permalink_url + track.title + ' - ' + track.genre));
+      $('#results').append($('<li></li>')
+      .text( 
+      'Track Title: ' + track.title + ' || ' +
+      ' Genre: ' + track.genre + ' || ' +
+      ' Listen: ' + track.permalink_url));
+       });
     });
   });
 });
